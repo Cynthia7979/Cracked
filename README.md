e9098d4526844f3ed20eef035fda25a9df38584c
Cynthia Wang <CynthiaWang7979@hotmail.com>
2018-02-12 11:29:51 +0800

    Finishing abstract base class Mob and Skill. Updated README.md again.

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 <b>Cracked</b>, a game with imagination which players need to kill monsters in different floors.
-
-It's called Cracked because all monsters are creepy and not understandable.
+ It's called Cracked because all monsters are creepy and not understandable.
 
 Concepts in game:
 -------------------
 1. Abstract position.
     For every floor, (0,0) is at the bottom left.
     If every floor has an axb size, then bottom right = (a,0), top left = (0,b), top right = (a,b)
-
 2. Game status.
-        A GameStatus instance contains global data and variables in game.
-        GameStatus instance will not store any local data for functions or other instances.
-
+    A GameStatus instance contains global data and variables in game.
+    GameStatus instance will not store any local data for functions or other instances.
 3. Screen position.
-        A screen position is the real position we use to blit surface objects.
-        It's the pygame-kind position.
---------------------
+    A screen position is the real position we use to blit surface objects.
+    It's the pygame-kind position.
+4. Player status.
+    A PlayerStatus instance contains local variables for player (e.g. floor)
+    This instance will be deleted anytime the player quit \"playing\",
+    which means the player died or just stopped to visit achievement house or store in game.
+    This is
+
+
+---
 To calculate screen position, we must know where the origin(0,0) of the floor is.
 This will be stored in the main GameStatus instance.
--- src/Cracked.py
+++ src/Cracked.py
@@ -1,33 +1,69 @@
 import pygame
 import logging
 import sys
+import abc
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
 
 def logged(class_):
     class_.logger = logging.getLogger(class_.__name__)
     return class_
+
 
 @logged
 class GameStatus:
     def __init__(self):
         self.logger.debug('Name of the logger: {0}'.format(self.__class__.__name__))
         self.scene = 'loading'
+        self.__dict__.update({
+            'floor': 1,
+            'scene': 'loading',
+        })
+        self.logger.warn('Please check the realization of initialization for class GameStatus')
 
-    def update(self,data):
+    def update(self, data):
         self.logger.warn('This may not be a very good realization for updating')
-        assert isinstance(data,dict), 'The argument given to method update() is not a dictionary'
+        assert isinstance(data, dict), 'The argument given to method update() is not a dictionary'
         for key in data.keys():
             if key not in self.__dict__.keys():
                 del data[key]
         self.__dict__.update(data)
 
+
 @logged
-class Mob:
+class Mob(metaclass=abc.ABCMeta):
     def __init__(self):
         self.blood = 0
-        self.images = {'left':[],'right':[],'attack':[],'die':[]}
-        self.pos = ()
+        self.images = {'left': [], 'right': [], 'attack': [], 'die': []}
+        self.pos = ()  # Abstract position
+
+    @abc.abstractmethod
+    def make_policy(self):
+        """Decide what to do (attack, move, die) via current situation"""
+        pass
 
 
+@logged
+class Skill(metaclass=abc.ABCMeta):
+    def __init__(self):
+        self.image = None
+        self.buff = None
+        self.harm = 0
+
+    @abc.abstractmethod
+    def activate(self):
+        """Activate the skill"""
+        pass
+
+
+class Buff:
+    def __init__(self, name, image, time,
+                 blood_effect=0,
+                 speed_effect=0,
+                 harm_effect=0):
+        self.name = name
+        self.image = image
+        self.time = time
+        self.effect = {'blood': blood_effect, 'speed': speed_effect, 'harm': harm_effect}
 if __name__ == '__main__':
-    GameStatus().update({'scene':'opening'})
+    GameStatus().update({'scene': 'opening'})
